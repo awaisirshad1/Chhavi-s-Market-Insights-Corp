4cf36d609febf502bb14a0aa5bd2e67cb1c33616
awaisirshad1 <awaisirshad1@icloud.com>
2024-06-10 13:55:45 -0400

    Initial commit

---
+++ backend/src/main/java/org/Chhavi/CMIC_backend/CmicBackendApplication.java
@@ -0,0 +1,13 @@
+package org.Chhavi.CMIC_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmicBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmicBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/org/Chhavi/CMIC_backend/CmicBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.Chhavi.CMIC_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmicBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
