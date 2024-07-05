b1675f2c3d0ae82dc199a360941be2e2ab428261
Chi <lanchiph@student.ubc.ca>
2024-07-05 14:03:44 -0400

    pie chart added

--- frontend/script.js
+++ frontend/script.js
@@ -419,3 +419,73 @@ Highcharts.chart('container', {
   }
 
 });
+
+Highcharts.chart('pie-container', {
+  chart: {
+      type: 'pie'
+  },
+  title: {
+      text: 'Market Composition'
+  },
+  tooltip: {
+      valueSuffix: '%'
+  },
+  // subtitle: {
+  //     text:
+  //     ''
+  // },
+  plotOptions: {
+      series: {
+          allowPointSelect: true,
+          cursor: 'pointer',
+          dataLabels: [{
+              enabled: true,
+              distance: 20
+          }, {
+              enabled: true,
+              distance: -40,
+              format: '{point.percentage:.1f}%',
+              style: {
+                  fontSize: '1.2em',
+                  textOutline: 'none',
+                  opacity: 0.7
+              },
+              filter: {
+                  operator: '>',
+                  property: 'percentage',
+                  value: 10
+              }
+          }]
+      }
+  },
+  series: [
+      {
+          name: 'Percentage',
+          colorByPoint: true,
+          data: [
+              {
+                  name: 'Microsoft',
+                  y: 55.02
+              },
+              {
+                  name: 'Amazon',
+                  sliced: true,
+                  selected: true,
+                  y: 26.71
+              },
+              {
+                  name: 'Google',
+                  y: 1.09
+              },
+              {
+                  name: 'Tesla',
+                  y: 15.5
+              },
+              {
+                  name: 'Apple',
+                  y: 1.68
+              }
+          ]
+      }
+  ]
+});
